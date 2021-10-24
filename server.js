f962a4cf5f3caac181dc8c330566a3f31744a115
myogic <moh.yogi10065@gmail.com>
2021-10-24 22:01:50 +0700

    add server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//test route
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to the localhost:3000"
+    });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server was running on localhost ${PORT}`);
+});
