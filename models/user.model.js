a191597bc400cf1db80df9a548bb0e000d49545b
myogic <moh.yogi10065@gmail.com>
2021-10-24 22:37:51 +0700

    add models

---
+++ models/user.model.js
@@ -0,0 +1,14 @@
+module.exports = (squelize, Sequelize) => {
+    const User = squelize.define("users", {
+        username: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        }
+    });
+    return User;
+}
--- server.js
+++ server.js
@@ -13,6 +13,31 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require('./models');
+const Role = db.role;
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
+
 //test route
 app.get("/", (req, res) => {
     res.json({
